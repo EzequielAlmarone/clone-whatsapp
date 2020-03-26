21fb43841bf460a8440cabbc0c7c00d0b7a8bee7
Ezequiel Almarone <ezequiel.almarone@gmail.com>
2020-03-25 20:22:22 -0400

    Teste de comunicação com o firebase

--- app/src/main/java/com/almarone/whatsapp/MainActivity.java
+++ app/src/main/java/com/almarone/whatsapp/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class MainActivity extends AppCompatActivity {
+    private DatabaseReference firebaseReferencia = FirebaseDatabase.getInstance().getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firebaseReferencia.child("pontos").setValue(100);
     }
 }
